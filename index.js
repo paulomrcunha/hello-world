08073063462993335830457a5bce469cc035a2da
Paulo Cunha <paulomrodriguescunha@gmail.com>
2018-08-23 15:03:38 +0100

    Add index.js file to the project.

---
+++ index.js
@@ -0,0 +1,15 @@
+console.log('Hello World was the name of this git project.');
+
+// Object
+const circle = {
+  radius: 1,
+  location: {
+    x: 1,
+    y: 1
+  },
+  draw: function() {
+    console.log('draw');
+  }
+};
+
+circle.draw;
