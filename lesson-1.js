8ad5a2a16ae63c077732161d435753ed1dc02fc1
Paulo Cunha <paulomrodriguescunha@gmail.com>
2018-08-23 17:44:56 +0100

    Change to a proper name.

---
+++ lesson-1.js
@@ -0,0 +1,14 @@
+// Create a Object
+const circleOne = {
+  radius: 1,
+  location: {
+    x: 1,
+    y: 1
+  },
+  draw: function () {
+    // Open the browser console
+    console.log('draw');
+  }
+};
+
+circleOne.draw();
